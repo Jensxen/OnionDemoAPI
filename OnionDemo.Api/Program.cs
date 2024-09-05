de62eb3faa2f43000b841e1f77e6165caadac15c
M. <146251152+Jensxen@users.noreply.github.com>
2024-09-05 13:28:30 +0200

    chat we are cooking

--- OnionDemo.Api/Program.cs
+++ OnionDemo.Api/Program.cs
@@ -1,10 +1,8 @@
-using Microsoft.EntityFrameworkCore;
 using OnionDemo.Application;
 using OnionDemo.Application.Command;
+using OnionDemo.Application.Command.CommandDto;
 using OnionDemo.Application.Query;
-using OnionDemo.Domain.DomainServices;
 using OnionDemo.Infrastructure;
-using OnionDemo.Infrastructure.Queries;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,23 +10,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<IBookingCommand, BookingCommand>();
-builder.Services.AddScoped<IBookingQuery, BookingQuery>();
-builder.Services.AddScoped<IBookingDomainService, BookingDomainService>();
 
-// Database
-// https://github.com/dotnet/SqlClient/issues/2239
-// https://learn.microsoft.com/en-us/ef/core/managing-schemas/migrations/projects?tabs=dotnet-core-cli
-// Add-Migration InitialMigration -Context BookMyHomeContext -Project OnionDemo.DatabaseMigration
-// Update-Database -Context BookMyHomeContext -Project OnionDemo.DatabaseMigration
-builder.Services.AddDbContext<BookMyHomeContext>(options =>
-    options.UseSqlServer(
-        builder.Configuration.GetConnectionString
-            ("BookMyHomeDbConnection"),
-        x =>
-            x.MigrationsAssembly("OnionDemo.DatabaseMigration")));
-
-builder.Services.AddScoped<IBookingRepository, BookingRepository>();
+// Application and Infrastructure services
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
@@ -41,31 +26,12 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
 // https://learn.microsoft.com/en-us/aspnet/core/tutorials/min-web-api?view=aspnetcore-8.0&tabs=visual-studio
+app.MapGet("/hello", () => "Hello World");
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+app.MapGet("/booking", (IBookingQuery query) => query.GetBookings());
+app.MapGet("/booking/{id}", (int id, IBookingQuery query) => query.GetBooking(id));
+app.MapPost("/booking", (CreateBookingDto booking, IBookingCommand command) => command.CreateBooking(booking));
+app.MapPut("/booking", (UpdateBookingDto booking, IBookingCommand command) => command.UpdateBooking(booking));
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
--- OnionDemo.Infrastructure/BookingRepository.cs
+++ OnionDemo.Infrastructure/BookingRepository.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using OnionDemo.Application;
 using OnionDemo.Domain.Entity;
 
@@ -29,9 +31,47 @@ namespace OnionDemo.Infrastructure
 
         void IBookingRepository.UpdateBooking(Booking booking, byte[] rowversion)
         {
+            using (var transaction = _db.Database.BeginTransaction(IsolationLevel.Serializable))
+            {
+                try
+                {
+                    _db.Entry(booking).Property(nameof(booking.RowVersion)).OriginalValue = rowversion;
+                    _db.SaveChanges();
+                    transaction.Commit();
+                }
+                catch (DbUpdateConcurrencyException ex)
+                {
+                    transaction.Rollback();
 
-            _db.Entry(booking).Property(nameof(booking.RowVersion)).OriginalValue = rowversion;
-            _db.SaveChanges();
+                    var entry = _db.Entry(booking);
+                    var databaseEntry = entry.GetDatabaseValues();
+
+                    if (databaseEntry == null)
+                    {
+                        throw new Exception("The booking was deleted by another user");
+                    }
+                    else
+                    {
+                        var databaseValues = (Booking)databaseEntry.ToObject();
+                        entry.OriginalValues.SetValues(databaseValues);
+                        entry.CurrentValues.SetValues(booking);
+
+                        //Update the rowversion to the current value
+                        entry.Property(nameof(booking.RowVersion)).OriginalValue = databaseValues.RowVersion;
+
+                        _db.SaveChanges();
+                        
+                    }
+
+                }
+                catch (Exception)
+                {
+                    transaction.Rollback();
+                    throw;
+                }
+            }
+
+
         }
     }
 }
